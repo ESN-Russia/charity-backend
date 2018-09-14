5d5bf0b15de7e8981a0f4457150f6eb9990ec0b0
Andrey Sokolov <sokolov-as-i@yandex.ru>
2018-09-14 12:40:07 +0300

    update dependencies

--- esncharity/settings.py
+++ esncharity/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'esncharity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
