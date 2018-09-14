d19d6b5a561d32179478a001d312706900bae628
Andrey Sokolov <sokolov-as-i@yandex.ru>
2018-09-14 13:07:23 +0300

    update deps, add base model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class CharityLot(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True)
+
+    base_cost = models.IntegerField(default=0)
+    start_time = models.DateTimeField(blank=True)
+    end_time = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return str(self.name)
