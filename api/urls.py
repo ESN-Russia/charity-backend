974da749dbedf5bbc7534f6c280e0d1e98ade973
Andrey Sokolov <sokolov-as-i@yandex.ru>
2018-10-06 02:45:47 +0300

    add login and logout logic

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from knox import views as knox_views
 from .views import *
 
 urlpatterns = [
     path('lot-list/', CharityLotsList.as_view()),
+
+    path('auth/login/', AuthLoginView.as_view()),
+    path('auth/logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import login
 
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from knox.auth import TokenAuthentication
+from knox.views import LoginView as KnoxLoginView
 
 from .serializers import *
 
 class CharityLotsList(generics.ListAPIView):
+    # permission_classes = (AllowAny,)
+
     queryset = CharityLot.objects.all()
     serializer_class = CharityLotSerializer
+
+
+class AuthLoginView(KnoxLoginView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(AuthLoginView, self).post(request, format=None)
